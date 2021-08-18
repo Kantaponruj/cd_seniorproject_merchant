a043f4723d253376794529c81cf3eb51e80f4e55
Krititya Onbun-ua <krittiya.on@mail.kmutt.ac.th>
2021-08-18 19:40:00 +0700

    [ADD] storeNotifier

---
+++ lib/notifiers/store_notifier.dart
@@ -0,0 +1,121 @@
+import 'package:cs_senior_project_merchant/asset/constant.dart';
+import 'package:cs_senior_project_merchant/models/store.dart';
+import 'package:cs_senior_project_merchant/services/store_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+enum Status { Uninitialized, Authenticated, Authenticating, Unauthenticated }
+
+class StoreNotifier with ChangeNotifier {
+  User _user;
+  Status _status = Status.Uninitialized;
+  StoreService _storeService = StoreService();
+  Store _store;
+
+  User get user => _user;
+  Status get status => _status;
+  Store get store => _store;
+
+  final formkey = GlobalKey<FormState>();
+
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+  TextEditingController confirmPassword = TextEditingController();
+  TextEditingController displayName = TextEditingController();
+
+  StoreNotifier.initialize() {
+    _fireSetUp();
+  }
+
+  _fireSetUp() async {
+    await initialization.then((value) {
+      auth.authStateChanges().listen(_onStateChange);
+    });
+  }
+
+  Future<bool> signIn() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    try {
+      _status = Status.Authenticating;
+      notifyListeners();
+      await auth
+          .signInWithEmailAndPassword(
+              email: email.text.trim(), password: password.text.trim())
+          .then((value) async {
+        await prefs.setString("storeId", value.user.uid);
+      });
+      return true;
+    } catch (e) {
+      _status = Status.Unauthenticated;
+      notifyListeners();
+      print(e.toString());
+      return false;
+    }
+  }
+
+  Future<bool> signUp() async {
+    try {
+      _status = Status.Authenticating;
+      notifyListeners();
+      await auth
+          .createUserWithEmailAndPassword(
+              email: email.text.trim(), password: password.text.trim())
+          .then((result) async {
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        // String _deviceToken = await fcm.getToken();
+        await prefs.setString("uid", result.user.uid);
+        _storeService.createUser(
+          storeId: result.user.uid,
+          email: email.text.trim(),
+        );
+      });
+      return true;
+    } catch (e) {
+      _status = Status.Unauthenticated;
+      notifyListeners();
+      print(e.toString());
+      return false;
+    }
+  }
+
+  Future signOut() async {
+    auth.signOut();
+    _status = Status.Unauthenticated;
+    notifyListeners();
+    return Future.delayed(Duration.zero);
+  }
+
+  void clearController() {
+    displayName.text = "";
+    email.text = "";
+    password.text = "";
+    confirmPassword.text = "";
+  }
+
+  Future<void> reloadUserModel() async {
+    _store = await _storeService.getUserById(user.uid);
+    notifyListeners();
+  }
+
+  updateUserData(Map<String, dynamic> data) async {
+    _storeService.updateUserData(data);
+  }
+
+  _onStateChange(User firebaseUser) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (firebaseUser == null) {
+      _status = Status.Unauthenticated;
+    } else {
+      _user = firebaseUser;
+      await prefs.setString("storeId", firebaseUser.uid);
+
+      _store = await _storeService.getUserById(user.uid).then((value) {
+        _status = Status.Authenticated;
+        return value;
+      });
+    }
+    notifyListeners();
+  }
+}
